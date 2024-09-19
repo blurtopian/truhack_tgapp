e55ee7fc8b2682375dc4fd672e4cfdb99a03eb79
East <eastmaels@gmail.com>
2024-09-19 13:11:54 +0900

    fix ts errors

--- src/helpers/functions.tsx
+++ src/helpers/functions.tsx
@@ -1,6 +1,6 @@
 import moment from 'moment';
 
-export default function disablePastDates(current) {
+export default function disablePastDates(current: any) {
   // Can not select days before today and today
   return current && current < moment().endOf('day');
 }
