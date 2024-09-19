e4861ca48f22f0f06c847123c32d5a8732063868
East <eastmaels@gmail.com>
2024-09-19 14:50:18 +0900

    update API URL

--- src/apis/truhackAPI.tsx
+++ src/apis/truhackAPI.tsx
@@ -1,6 +1,6 @@
 import Client from '../helpers/client'
 
-const url = import.meta.env.REACT_APP_TRUHACK_API || 'http://localhost:3002';
+const url = import.meta.env.REACT_APP_TRUHACK_API || 'https://blurtopian.com/truhack_tgapp/';
 const truhackAPI = new Client(url);
 
 export default truhackAPI;
