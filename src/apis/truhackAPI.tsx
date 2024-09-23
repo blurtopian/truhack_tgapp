83e9cf1a99478a9e5abbe23281d0c6d400015cc2
East <eastmaels@gmail.com>
2024-09-23 23:42:12 +0900

    update API URL

--- src/apis/truhackAPI.tsx
+++ src/apis/truhackAPI.tsx
@@ -1,6 +1,6 @@
 import Client from '../helpers/client'
 
-const url = import.meta.env.VITE_TRUHACK_API || 'https://truhack-api-c3b4ef73d73b.herokuapp.com';
+const url = import.meta.env.VITE_TRUHACK_API || 'https://truhackapi.blurtopian.com';
 const truhackAPI = new Client(url);
 
 export default truhackAPI;
